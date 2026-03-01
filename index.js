cf2c574d4cd6c1c3955da0b5950720b1cc00ca06
sanjan123-prog <chinthalasanjanreddy@gmail.com>
2026-03-01 11:15:46 +0530

    final render fix

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Global Notes API Running");
+});
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-    console.log("MongoDB Connected");
+  console.log("MongoDB Connected");
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log("Server running on port " + PORT));
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 })
-.catch(err => console.log(err));
+.catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
