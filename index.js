26d93f0a6da23f281c8cc1b1f41d4abd25159fd5
sanjan123-prog <chinthalasanjanreddy@gmail.com>
2026-03-01 10:53:34 +0530

    backend ready

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running"));
-mongoose.connect(process.env.MONGO_URI);
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log("Server running on port " + PORT));
+})
+.catch(err => console.log(err));
